4581671817094a7ca0ca2dd9f4828cbfe5279cdc
Ethan Frot <contact@ethanfrot.com>
2026-02-02 15:21:09 +0100

    🐛 Add cors for http connexions

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import {
@@ -13,6 +14,7 @@ const io = new Server(httpServer, {
   cors: { origin: "*" },
 });
 
+app.use(cors());
 app.use(express.json());
 
 // List connected mini-games
