683690b72399e2aa97ad65c26aab21581d428c21
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2026-02-05 06:32:41 +0100

    fix: show studant card

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
+// GameMaster
 export const API_URL = "http://192.168.10.1:3000"; // ESD MAC
 // export const API_URL = "http://192.168.1.46:3000"; // THOMAS HOME
 // export const API_URL = "http://10.14.73.40:3000"; // THOMAS ESD
